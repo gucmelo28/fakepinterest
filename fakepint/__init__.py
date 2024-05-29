3421f2905558168ec7624e96388f30fd42054cb5
gucmelo <gustavo.melo@vtex.com>
2024-05-28 21:49:04 -0300

    adjust database creation

--- fakepint/__init__.py
+++ fakepint/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "d5dbbfa42967857a14e9a2b99a9a4f51"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
